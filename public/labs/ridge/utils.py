f68fbd472d4f3a30814b892a8a9f0aed586daf29
Liberty Hamilton <libertyhamilton@gmail.com>
2026-04-09 17:43:22 -0700

    Lab 10

---
+++ public/labs/ridge/utils.py
@@ -0,0 +1,119 @@
+
+import numpy as np
+#import scipy.stats
+import random
+import sys
+import h5py
+
+
+def make_delayed(stim, delays, circpad=False):
+    """Creates non-interpolated concatenated delayed versions of [stim] with the given [delays] 
+    (in samples).
+    
+    If [circpad], instead of being padded with zeros, [stim] will be circularly shifted.
+    """
+    nt,ndim = stim.shape
+    dstims = []
+    for di,d in enumerate(delays):
+        dstim = np.zeros((nt, ndim))
+        if d<0: ## negative delay
+            dstim[:d,:] = stim[-d:,:]
+            if circpad:
+                dstim[d:,:] = stim[:-d,:]
+        elif d>0:
+            dstim[d:,:] = stim[:-d,:]
+            if circpad:
+                dstim[:d,:] = stim[-d:,:]
+        else: ## d==0
+            dstim = stim.copy()
+        dstims.append(dstim)
+    return np.hstack(dstims)
+
+
+def mult_diag(d, mtx, left=True):
+    """Multiply a full matrix by a diagonal matrix.
+    This function should always be faster than dot.
+
+    Input:
+      d -- 1D (N,) array (contains the diagonal elements)
+      mtx -- 2D (N,N) array
+
+    Output:
+      mult_diag(d, mts, left=True) == dot(diag(d), mtx)
+      mult_diag(d, mts, left=False) == dot(mtx, diag(d))
+    
+    By Pietro Berkes
+    From http://mail.scipy.org/pipermail/numpy-discussion/2007-March/026807.html
+    """
+    if left:
+        return (d*mtx.T).T
+    else:
+        return d*mtx
+
+import time
+import logging
+def counter(iterable, countevery=100, total=None, logger=logging.getLogger("counter")):
+    """Logs a status and timing update to [logger] every [countevery] draws from [iterable].
+    If [total] is given, log messages will include the estimated time remaining.
+    """
+    start_time = time.time()
+
+    ## Check if the iterable has a __len__ function, use it if no total length is supplied
+    if total is None:
+        if hasattr(iterable, "__len__"):
+            total = len(iterable)
+    
+    for count, thing in enumerate(iterable):
+        yield thing
+        
+        if not count%countevery:
+            current_time = time.time()
+            rate = float(count+1)/(current_time-start_time)
+
+            if rate>1: ## more than 1 item/second
+                ratestr = "%0.2f items/second"%rate
+            else: ## less than 1 item/second
+                ratestr = "%0.2f seconds/item"%(rate**-1)
+            
+            if total is not None:
+                remitems = total-(count+1)
+                remtime = remitems/rate
+                timestr = ", %s remaining" % time.strftime('%H:%M:%S', time.gmtime(remtime))
+                itemstr = "%d/%d"%(count+1, total)
+            else:
+                timestr = ""
+                itemstr = "%d"%(count+1)
+
+            formatted_str = "%s items complete (%s%s)"%(itemstr,ratestr,timestr)
+            if logger is None:
+                print(formatted_str)
+            else:
+                logger.info(formatted_str)
+
+def check_alphas(allRcorrs, alphas):
+	'''
+	Check that the alpha parameters are in range for this dataset.
+	You know they are in a good range if the correlation performance
+	peaks in the middle (not at the smallest alpha nor at the
+	largest alpha value).
+
+	Inputs:
+		allRcorrs : matrix of bootstrapped correlations across all
+				 regularization values (alphas). This will be
+				 of dimensions [nalphas x nchannels x nboots]
+		alphas : vector of values for the regularization
+				 coefficients (for plotting).
+	'''
+	print('Checking that regularization alphas are in range')
+	# bcorrs is nalphas x nchans x nboots
+	fig = plt.figure()
+	plt.semilogx(alphas, allRcorrs.mean(2))  # Log space alphas
+	# Plot a line at the maximum alpha. This should be in the middle
+	plt.axvline(alphas[allRcorrs.mean(2).mean(1).argmax()])
+	plt.xlabel('Alpha')
+	plt.ylabel('Correlation')
+	plt.show()
+
+	return fig
+
+
